695265bcf1dbd3158c2114fe46bdbf0a17209dc7
jlesperance <jlesperance@8d88f056-6564-4690-97fc-8730f0e09817>
2009-12-11 23:28:22 +0000

    Mobed the location of the default module's controllers
    git-svn-id: https://eternalcms.svn.sourceforge.net/svnroot/eternalcms/trunk@79 8d88f056-6564-4690-97fc-8730f0e09817

---
+++ application/controllers/AddController.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * AddController
+ * 
+ * @author
+ * @version 
+ */
+require_once 'eCMS/Controller/Action.php';
+class AddController extends eCMS_Controller_Action
+{
+    /**
+     * The default action - show the home page
+     */
+	public function init(){
+		parent::init();
+	}
+    public function indexAction ()
+    {
+        $this->view->title = "Add Article";
+        $this->view->bodyCopy = "<p>Fill out the following form to add an article.</p><p>The summary can be up to 250 characters long</p>";
+        $form = new Default_Form_AddNews();
+        if ($this->_request->isPost()) {
+            $formData = $this->_request->getPost();
+            if ($form->isValid($formData)) {
+		$data = $form->getValues();
+                $data['author'] = ($this->view->user)?$this->view->user->uname : 'Anonymous';
+		$data['summary'] = nl2br($data['summary']);
+		$data['content'] = nl2br($data['content']);
+                $news = new Default_Model_News($data);
+		$news->save();
+                $this->_redirect('view/latest');
+            } else {
+                $form->populate($formData);
+                $this->view->form = $form;
+            }
+        } else {
+            $this->view->form = $form;
+        }
+    }
+}
---
+++ application/controllers/ViewController.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * ViewController
+ * 
+ * @author
+ * @version 
+ */
+require_once 'eCMS/Controller/Action.php';
+class ViewController extends eCMS_Controller_Action
+{
+    /**
+     * The default action - show the home page
+     */
+	public function init(){
+		parent::init();
+		$this->news = new Default_Model_News();
+		$this->settings = Zend_Registry::get('settings');
+		$this->view->footerTitle = $this->settings->footer->title;
+	    $this->view->footerLink = $this->settings->footer->link;
+
+	}
+    public function indexAction ()
+    {
+        $this->_helper->redirector('latest');
+    }
+    public function latestAction ()
+    {
+        $this->view->title = 'Latest News';
+        $this->view->news = $this->news->fetchLatest((int)$this->settings->limit->latest);
+    }
+    public function popularAction ()
+    {
+        $this->view->title = 'Most Popular';
+        $this->view->news = $this->news->fetchPopular((int)$this->settings->limit->popular);
+    }
+    public function allAction ()
+    {
+        $limit = (int)$this->settings->limit->perpage;
+        $this->view->title = 'All News';
+        $page = $this->_request->getParam('page', 1);
+	$paginator = $this->news->fetchPage($page, $limit);
+	$this->view->news = $paginator->getItemsByPage($page);
+        $this->view->paginator = $paginator;
+    }
+    public function cssAction ()
+    {}
+    
+    public function articleAction(){
+        $id = $this->_request->getParam('sid');
+        $this->news->find($id);
+	$this->view->title = $this->news->getTitle();
+	$this->view->news = $this->news;
+        $this->news->addView();
+    }
+}
