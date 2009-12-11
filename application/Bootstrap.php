3f1fe67a831c5133436e0ff8957d97adb3c4461f
jlesperance <jlesperance@8d88f056-6564-4690-97fc-8730f0e09817>
2009-12-11 22:44:17 +0000

    Removed the old bootstrap ffile and Initializer class and add a Zend Application Bootstrap file
    git-svn-id: https://eternalcms.svn.sourceforge.net/svnroot/eternalcms/trunk@74 8d88f056-6564-4690-97fc-8730f0e09817

---
+++ application/Bootstrap.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Application bootstrap
+ * 
+ * @uses    Zend_Application_Bootstrap_Bootstrap
+ * @package QuickStart
+ */
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
+{
+    /**
+     * Bootstrap autoloader for application resources
+     * 
+     * @return Zend_Application_Module_Autoloader
+     */
+    protected function _initAutoload()
+    {
+        $autoloader = new Zend_Application_Module_Autoloader(array(
+            'namespace' => 'Default',
+            'basePath'  => dirname(__FILE__),
+        ));
+        return $autoloader;
+    }
+
+    /**
+     * Bootstrap the view doctype
+     * 
+     * @return void
+     */
+    protected function _initDoctype()
+    {
+        $this->bootstrap('view');
+        $view = $this->getResource('view');
+        $view->doctype('XHTML1_STRICT');
+    }
+	
+	protected function _initSettings(){
+		$settings = new Zend_Config_Ini(APPLICATION_PATH . "/configs/siteSettings.ini","general");
+		Zend_Registry::set('settings', $settings);
+	}
+	
+	protected function _initHelpers(){
+		Zend_Controller_Action_HelperBroker::addPrefix('eCMS_Controller_Action_Helper');
+	}
+}
