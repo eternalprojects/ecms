e0a8eac9c94a5f1dff778174a2a6443c661bdbac
jlesperance <jlesperance@8d88f056-6564-4690-97fc-8730f0e09817>
2009-12-19 00:15:52 +0000

    Changed the CSS to an external file
    git-svn-id: https://eternalcms.svn.sourceforge.net/svnroot/eternalcms/trunk@88 8d88f056-6564-4690-97fc-8730f0e09817

--- application/forms/AddNews.php
+++ application/forms/AddNews.php
@@ -6,7 +6,8 @@ class Default_Form_AddNews extends Zend_Form
 		$this->setMethod('post');
         	$this->setAction('/add');
 		$this->addElement('text','title',array('required'=>true,'label'=>'Article Title','size'=>75,'validators'=>array('NotEmpty',array('stringLength',8)),'filters'=>array('StripTags','StringTrim')));
-        	$this->addElement('textarea','summary', array('required'=>true,'label'=>'Article Summary','rows'=>4,'validators'=>array('NotEmpty'),'filters'=>array('StripTags','StringTrim')));
+        	$this->addElement('textarea','summary', array('required'=>true,'label'=>'Article Summary','rows'=>4,'cols'=>80,'validators'=>array('NotEmpty'),'filters'=>array('StripTags','StringTrim')));
+
         	$this->addElement('textarea','content', array('required'=>true,'label'=>'Article Content','validators'=>array('NotEmpty'),'filters'=>array('StripTags','StringTrim')));
 	       	$this->addElement('captcha','captcha', array('label'=>'Please enter the code below','captcha'=>array('captcha'=>'image','wordlen'=>7,'timeout'=>300,'font'=>APPLICATION_PATH . "/configs/VERDANA.TTF")));
 		$this->addElement('submit','submit',array('label'=>'Add Article'));
