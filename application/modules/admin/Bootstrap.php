c575b36140f5b08a7e665f7f8e945774bd865c59
jlesperance <jlesperance@8d88f056-6564-4690-97fc-8730f0e09817>
2009-12-20 09:25:24 +0000

    Added the basic structure for the Admin Module
    git-svn-id: https://eternalcms.svn.sourceforge.net/svnroot/eternalcms/trunk@131 8d88f056-6564-4690-97fc-8730f0e09817

---
+++ application/modules/admin/Bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Application bootstrap
+ *
+ * @uses    Zend_Application_Bootstrap_Bootstrap
+ * @package QuickStart
+ */
+class Admin_Bootstrap extends Zend_Application_Module_Bootstrap
+{
+
+}
