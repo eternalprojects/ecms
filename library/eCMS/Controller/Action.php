2a971f019c25de2627a835dc7c09c95dcc29401a
jlesperance <jlesperance@8d88f056-6564-4690-97fc-8730f0e09817>
2009-12-20 09:15:45 +0000

    Moved the footer var assignments to the extended action
    git-svn-id: https://eternalcms.svn.sourceforge.net/svnroot/eternalcms/trunk@130 8d88f056-6564-4690-97fc-8730f0e09817

--- library/eCMS/Controller/Action.php
+++ library/eCMS/Controller/Action.php
@@ -8,6 +8,8 @@ class eCMS_Controller_Action extends Zend_Controller_Action {
 		$this->view->siteName = Zend_Registry::get('settings')->site->name;
 		$this->view->menu = $this->_helper->generateMenu(Zend_Auth::getInstance()->getIdentity());
 		$this->view->slogan = Zend_Registry::get('settings')->site->slogan;
+		$this->view->footLink = Zend_Registry::get('settings')->footer->link;
+		$this->view->footTitle = Zend_Registry::get('settings')->footer->title;
 	}
 }
 
