55307f96e2fdffe673ffe3413ae06ad282415e18
jlesperance <jlesperance@8d88f056-6564-4690-97fc-8730f0e09817>
2009-12-20 02:29:48 +0000

    Moving the Submissions controller in the Members Module over to using the News DAO classes properly, This checkin is for the View action with a change to the pagination also
    git-svn-id: https://eternalcms.svn.sourceforge.net/svnroot/eternalcms/trunk@100 8d88f056-6564-4690-97fc-8730f0e09817

--- application/models/News.php
+++ application/models/News.php
@@ -3,86 +3,86 @@
 /**
  * Guestbook model
  *
- * Utilizes the Data Mapper pattern to persist data. Represents a single 
+ * Utilizes the Data Mapper pattern to persist data. Represents a single
  * guestbook entry.
- * 
+ *
  * @uses       Default_Model_GuestbookMapper
  * @package    QuickStart
  * @subpackage Model
  */
 class Default_Model_News
 {
-    protected $_views;
-    protected $_modified;
-    protected $_created;
-    protected $_author;
+	protected $_views;
+	protected $_modified;
+	protected $_created;
+	protected $_author;
 	protected $_content;
 	protected $_summary;
 	protected $_title;
-	protected $_id; 
-    protected $_mapper;
+	protected $_id;
+	protected $_mapper;
 
-    /**
-     * Constructor
-     * 
-     * @param  array|null $options 
-     * @return void
-     */
-    public function __construct(array $options = null)
-    {
-        if (is_array($options)) {
-            $this->setOptions($options);
-        }
-    }
+	/**
+	 * Constructor
+	 *
+	 * @param  array|null $options
+	 * @return void
+	 */
+	public function __construct(array $options = null)
+	{
+		if (is_array($options)) {
+			$this->setOptions($options);
+		}
+	}
 
-    /**
-     * Overloading: allow property access
-     * 
-     * @param  string $name 
-     * @param  mixed $value 
-     * @return void
-     */
-    public function __set($name, $value)
-    {
-        $method = 'set' . $name;
-        if ('mapper' == $name || !method_exists($this, $method)) {
-            throw Exception('Invalid property specified');
-        }
-        $this->$method($value);
-    }
+	/**
+	 * Overloading: allow property access
+	 *
+	 * @param  string $name
+	 * @param  mixed $value
+	 * @return void
+	 */
+	public function __set($name, $value)
+	{
+		$method = 'set' . $name;
+		if ('mapper' == $name || !method_exists($this, $method)) {
+			throw Exception('Invalid property specified');
+		}
+		$this->$method($value);
+	}
 
-    /**
-     * Overloading: allow property access
-     * 
-     * @param  string $name 
-     * @return mixed
-     */
-    public function __get($name)
-    {
-        $method = 'get' . $name;
-        if ('mapper' == $name || !method_exists($this, $method)) {
-            throw new Exception('Invalid property specified');
-        }
-        return $this->$method();
-    }
+	/**
+	 * Overloading: allow property access
+	 *
+	 * @param  string $name
+	 * @return mixed
+	 */
+	public function __get($name)
+	{
+		$method = 'get' . $name;
+		if ('mapper' == $name || !method_exists($this, $method)) {
+			throw new Exception('Invalid property specified');
+		}
+		return $this->$method();
+	}
 
-    /**
-     * Set object state
-     * 
-     * @param  array $options 
-     * @return Default_Model_Guestbook
-     */
-    public function setOptions(array $options)
-    {
-        $methods = get_class_methods($this);
-        foreach ($options as $key => $value) {
-            $method = 'set' . ucfirst($key);
-            if (in_array($method, $methods)) {
-                $this->$method($value);
-            }
-        }
-        return $this;
-    }
+	/**
+	 * Set object state
+	 *
+	 * @param  array $options
+	 * @return Default_Model_Guestbook
+	 */
+	public function setOptions(array $options)
+	{
+		$methods = get_class_methods($this);
+		foreach ($options as $key => $value) {
+			$method = 'set' . ucfirst($key);
+			if (in_array($method, $methods)) {
+				$this->$method($value);
+			}
+		}
+		return $this;
+	}
 
 	public function setViews($views)
 	{
@@ -97,144 +97,144 @@ class Default_Model_News
 
 	public function setModified($ts)
 	{
-        $this->_modified = $ts; 
-        return $this;
-    }
+		$this->_modified = $ts;
+		return $this;
+	}
 
-    public function getModified()
-    {
-        return $this->_modified;
-    }
+	public function getModified()
+	{
+		return $this->_modified;
+	}
 
 	public function setCreated($ts)
 	{
-        $this->_created = $ts; 
-        return $this;
-    }
+		$this->_created = $ts;
+		return $this;
+	}
 
-    public function getCreated()
-    {
-        return $this->_created;
-    }
+	public function getCreated()
+	{
+		return $this->_created;
+	}
 
 	public function setAuthor($author)
 	{
-        $this->_author = $author; 
-        return $this;
-    }
+		$this->_author = $author;
+		return $this;
+	}
 
-    public function getAuthor()
-    {
-        return $this->_author;
-    }
+	public function getAuthor()
+	{
+		return $this->_author;
+	}
 
 	public function setContent($content)
 	{
-        $this->_content = $content; 
-        return $this;
-    }
+		$this->_content = $content;
+		return $this;
+	}
 
-    public function getContent()
-    {
-        return $this->_content;
-    }
+	public function getContent()
+	{
+		return $this->_content;
+	}
 
 	public function setSummary($summary)
 	{
-        $this->_summary = $summary; 
-        return $this;
-    }
+		$this->_summary = $summary;
+		return $this;
+	}
 
-    public function getSummary()
-    {
-        return $this->_summary;
-    }
+	public function getSummary()
+	{
+		return $this->_summary;
+	}
 
 	public function setTitle($title)
 	{
-        $this->_title = $title; 
-        return $this;
-    }
+		$this->_title = $title;
+		return $this;
+	}
 
-    public function getTitle()
-    {
-        return $this->_title;
-    }
+	public function getTitle()
+	{
+		return $this->_title;
+	}
 
 	public function setid($id)
 	{
-        $this->_id = $id; 
-        return $this;
-    }
+		$this->_id = $id;
+		return $this;
+	}
 
-    public function getId()
-    {
-        return $this->_id;
-    }
+	public function getId()
+	{
+		return $this->_id;
+	}
 
-    /**
-     * Set data mapper
-     * 
-     * @param  mixed $mapper 
-     * @return Default_Model_Guestbook
-     */
-    public function setMapper($mapper)
-    {
-        $this->_mapper = $mapper;
-        return $this;
-    }
+	/**
+	 * Set data mapper
+	 *
+	 * @param  mixed $mapper
+	 * @return Default_Model_Guestbook
+	 */
+	public function setMapper($mapper)
+	{
+		$this->_mapper = $mapper;
+		return $this;
+	}
 
-    /**
-     * Get data mapper
-     *
-     * Lazy loads Default_Model_GuestbookMapper instance if no mapper registered
-     * 
-     * @return Default_Model_GuestbookMapper
-     */
-    public function getMapper()
-    {
-        if (null === $this->_mapper) {
-            $this->setMapper(new Default_Model_NewsMapper());
-        }
-        return $this->_mapper;
-    }
+	/**
+	 * Get data mapper
+	 *
+	 * Lazy loads Default_Model_GuestbookMapper instance if no mapper registered
+	 *
+	 * @return Default_Model_GuestbookMapper
+	 */
+	public function getMapper()
+	{
+		if (null === $this->_mapper) {
+			$this->setMapper(new Default_Model_NewsMapper());
+		}
+		return $this->_mapper;
+	}
 
-    /**
-     * Save the current entry
-     * 
-     * @return void
-     */
-    public function save()
-    {
-        $this->getMapper()->save($this);
-    }
+	/**
+	 * Save the current entry
+	 *
+	 * @return void
+	 */
+	public function save()
+	{
+		$this->getMapper()->save($this);
+	}
 
 	public function addView()
 	{
 		$this->getMapper()->addView($this);
 	}
-    /**
-     * Find an entry
-     *
-     * Resets entry state if matching id found.
-     * 
-     * @param  int $id 
-     * @return Default_Model_Guestbook
-     */
-    public function find($id)
-    {
-        $this->getMapper()->find($id, $this);
-    }
+	/**
+	 * Find an entry
+	 *
+	 * Resets entry state if matching id found.
+	 *
+	 * @param  int $id
+	 * @return Default_Model_Guestbook
+	 */
+	public function find($id)
+	{
+		$this->getMapper()->find($id, $this);
+	}
 
-    /**
-     * Fetch all entries
-     * 
-     * @return array
-     */
-    public function fetchAll()
-    {
-        return $this->getMapper()->fetchAll();
-    }
+	/**
+	 * Fetch all entries
+	 *
+	 * @return array
+	 */
+	public function fetchAll()
+	{
+		return $this->getMapper()->fetchAll();
+	}
 
 	public function fetchLatest($limit = 10)
 	{
@@ -243,12 +243,15 @@ class Default_Model_News
 
 	public function fetchPopular($limit = 10)
 	{
-        return $this->getMapper()->fetchPopular($limit);
-    }
-	
+		return $this->getMapper()->fetchPopular($limit);
+	}
+
 	public function fetchPage($page = 1, $limit = 10, $style = 'elastic')
 	{
 		return $this->getMapper()->fetchPage($page, $limit, $style);
 	}
 
+	public function fetchNewsByAuthor($author, $page = 1, $limit = 10, $style = 'elastic'){
+		return $this->getMapper()->fetchNewsByAuthor($author, $page, $limit, $style);
+	}
 }
--- application/modules/members/controllers/SubmissionsController.php
+++ application/modules/members/controllers/SubmissionsController.php
@@ -2,9 +2,9 @@
 
 /**
  * SubmissionController
- * 
+ *
  * @author
- * @version 
+ * @version
  */
 
 require_once 'eCMS/Controller/Action.php';
@@ -13,34 +13,30 @@ class Members_SubmissionsController extends eCMS_Controller_Action {
 	/**
 	 * The default action - show the home page
 	 */
-	
+
 	public function init(){
 		parent::init();
 		if(!$this->view->user)
-			$this->_redirect('/members/auth/login');
-		
+		$this->_redirect('/members/auth/login');
+
 	}
 	public function indexAction() {
 		$this->_redirect('/members/submissions/view');
 	}
-	
+
 	public function viewAction(){
 		$this->view->title = "My Submissions";
 		$page = $this->_request->getParam('page', 1);
-        $news = new Default_Model_DbTable_News();
-		$select = $news->select()->where('author = ?',$this->view->user->uname);
-		$pagination = Zend_Paginator::factory($select);
-        $pagination->setCurrentPageNumber($page);
-        $pagination->setItemCountPerPage(10);
-        $pagination->setDefaultScrollingStyle('Elastic');
-        $this->view->news = $pagination->getItemsByPage($page);
-        $this->view->paginator = $pagination;
-		
+		$news = new Default_Model_News();
+		$paginator = $news->fetchNewsByAuthor($this->view->user->uname, $page);
+		$this->view->news = $paginator->getItemsByPage($page);
+		$this->view->paginator = $paginator;
+
 	}
-	
+
 	public function editAction(){
 		if(!$sid = (int)$this->_request->getParam('sid'))
-			$this->_redirect('/members/submissions/view');
+		$this->_redirect('/members/submissions/view');
 		$this->view->title = "Edit Article";
 		$news = new Default_Model_DbTable_News();
 		$form = new Default_Form_EditNews();
@@ -49,7 +45,7 @@ class Members_SubmissionsController extends eCMS_Controller_Action {
 		if($this->_request->isPost()){
 			$formData = $this->_request->getParams();
 			if($sid != $formData['id'])
-				$this->_redirect('/members/submissions/view');
+			$this->_redirect('/members/submissions/view');
 			if($form->isValid($formData)){
 				$data = array(
 					'title'=>$form->getValue('title'),
@@ -61,18 +57,18 @@ class Members_SubmissionsController extends eCMS_Controller_Action {
 				$this->_redirect('/view/article/sid/'.$sid);
 			}
 		}
-		
+
 		$form->getElement('title')->setValue($row[0]['title']);
 		$form->getElement('summary')->setValue($row[0]['summary']);
 		$form->getElement('content')->setValue($row[0]['content']);
 		$form->getElement('id')->setValue($sid);
 		$this->view->form = $form;
 	}
-	
+
 	public function deleteAction(){
 		$this->view->title = "Delete Article";
 		if(!$sid = (int)$this->_request->getParam('sid'))
-			$this->_redirect('/members/submissions/view');
+		$this->_redirect('/members/submissions/view');
 		if($this->_request->getParam('confirm') && $this->_request->getParam('confirm') == 1){
 			$this->_removeArticle($sid);
 			$this->_redirect('/members/submissions/view');
@@ -82,15 +78,15 @@ class Members_SubmissionsController extends eCMS_Controller_Action {
 		$this->view->sid = $row[0]['id'];
 		$this->view->articleTitle = $row[0]['title'];
 	}
-	
-	// This needs to be moved to the News DAO/Mapper 	
+
+	// This needs to be moved to the News DAO/Mapper
 	private function _removeArticle($sid){
 		$news = new Default_Model_DbTable_News();
 		$row = $news->find($sid);
 		if($this->view->user->uname != $row[0]['author'])
-			$this->_redirect('/members/submissions/view');
+		$this->_redirect('/members/submissions/view');
 		$where = $news->getAdapter()->quoteInto('id = ?',$sid);
-		$news->delete($where);	
+		$news->delete($where);
 	}
 
 }
