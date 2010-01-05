80815f13eed21e21266d08235bdb7be7ff44571d
jlesperance <jlesperance@8d88f056-6564-4690-97fc-8730f0e09817>
2010-01-05 22:13:57 +0000

    Added comments
    git-svn-id: https://eternalcms.svn.sourceforge.net/svnroot/eternalcms/trunk@153 8d88f056-6564-4690-97fc-8730f0e09817

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,15 +1,50 @@
 <?php
 /**
+ * Contains the Application Bootstrapping data
+ *
+ * License:
+ *
+ * Copyright (c) 2009, JPL Web Solutions,
+ *                     Jesse Lesperance <jesse@jplesperance.com>
+ *
+ * This file is part of EternalCMS.
+ *
+ * EternalCMS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.  EternalCMS is distributed in the hope
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details. You should have received
+ * a copy of the GNU General Public License along with EternalCMS.
+ *
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package Boostrap
+ * @subpackage Bootstrap
+ * @author Jesse Lesperance <jesse@jplesperance.com>
+ * @copyright 2010 JPL Web Solutions
+ * @license http://www.gnu.org/licenses/gpl-3.0-standalone.html GNU General Public License
+ *
+ */
+/**
  * Application bootstrap
  *
+ * This is where the initialization code for the Autoloader, Views, Helpers, 
+ * Routes and Settings are.
+ * 
  * @uses    Zend_Application_Bootstrap_Bootstrap
- * @package QuickStart
+ * @package Bootstrap
+ * @subpackage Bootstrap
+ * @author Jesse Lesperance
  */
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
     /**
      * Bootstrap autoloader for application resources
      *
+     * @access protected
      * @return Zend_Application_Module_Autoloader
      */
     protected function _initAutoload ()
@@ -20,7 +55,8 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
     /**
      * Bootstrap the view doctype
      *
-     * @return void
+     * @access protected
+     * @return Zend_View
      */
     protected function _initView ()
     {
@@ -32,17 +68,27 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $viewRenderer->setView($view);
         return $view;
     }
+    /**
+     * Bootstrap the site settings configuration
+     * 
+     * @access protected
+     */
     protected function _initSettings ()
     {
         $settings = new Zend_Config_Ini(APPLICATION_PATH . "/configs/siteSettings.ini", "general");
         Zend_Registry::set('settings', $settings);
     }
+    /**
+     * Initialize Action Helpers
+     */
     protected function _initHelpers ()
     {
         Zend_Controller_Action_HelperBroker::addPrefix('eCMS_Controller_Action_Helper');
     }
-    
-    protected function _initSiteRoutes()
+    /**
+     * Initialize the custom route definitions
+     */
+    protected function _initSiteRoutes ()
     {
         //Don't forget to bootstrap the front controller as the resource may not been created yet...
         $this->bootstrap("frontController");
--- application/controllers/AddController.php
+++ application/controllers/AddController.php
@@ -1,19 +1,63 @@
 <?php
 /**
- * AddController
+ * Controller for adding News
  *
- * @author
- * @version
+ * License:
+ *
+ * Copyright (c) 2009, JPL Web Solutions,
+ *                     Jesse Lesperance <jesse@jplesperance.com>
+ *
+ * This file is part of EternalCMS.
+ *
+ * EternalCMS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.  EternalCMS is distributed in the hope
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details. You should have received
+ * a copy of the GNU General Public License along with EternalCMS.
+ *
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package News
+ * @subpackage Controller
+ * @author Jesse Lesperance <jesse@jplesperance.com>
+ * @copyright 2010 JPL Web Solutions
+ * @license http://www.gnu.org/licenses/gpl-3.0-standalone.html GNU General Public License
+ *
  */
-require_once 'eCMS/Controller/Action.php';
+/**
+ * Controller for handling adding news
+ * 
+ * This controller contains the functionality for displaying the add news form, 
+ * validating the data and calling the News Model for inserting the submitted 
+ * news story into the database.
+ *
+ * @author Jesse Lesperance <jesse@jplesperance.com>
+ * @package News
+ * @subpackage Controller
+ * @uses eCMS_Controller_Action
+ */
 class AddController extends eCMS_Controller_Action
 {
 	/**
-	 * The default action - show the home page
+	 * Call the parent init function
 	 */
 	public function init(){
 		parent::init();
 	}
+	/**
+	 * The primary/default action
+	 * 
+	 * This method will dis[play the add news form if it does not detect the 
+	 * form already being submitted.  If the form has been submitted it validates 
+	 * the submitted data and upon validation it then calls the News model to 
+	 * insert the submitted data into the database
+	 * 
+	 * @
+	 */
 	public function indexAction ()
 	{
 		$this->view->title = "Add Article";
