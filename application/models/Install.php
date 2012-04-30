ad236b57db676c29e889c54c16dade73abdd444a
Jesse P Lesperance <jesse@jplesperance.me>
2012-04-29 21:42:14 -0700

    ECMS-14 ECMS-15
    Modified the Default_Model_Install to include the cat_id column when creating the news table and to insert a default 'uncategorized' category to the categories table during installation

--- application/models/Install.php
+++ application/models/Install.php
@@ -1,15 +1,16 @@
 <?php
 class Default_Model_Install
 {
-    public static function createTables ($db)
+    public static function createTables($db)
     {
         $sql = "CREATE TABLE IF NOT EXISTS `news` (
   			`id` int(11) NOT NULL AUTO_INCREMENT,
+  			`cat_id` int(11) NOT NULL DEFAULT '1',
 			  `title` varchar(64) NOT NULL,
 			  `summary` varchar(255) NOT NULL,
 			  `content` longtext NOT NULL,
 			  `author` varchar(128) NOT NULL,
-			  `cat_id` int(11) NOT NULL DEFAULT '1',
+
 			  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
 			  `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
 			  `views` int(11) NOT NULL,
@@ -31,10 +32,14 @@ class Default_Model_Install
 			  PRIMARY KEY (`id`)
 			) ENGINE=MyISAM  DEFAULT CHARSET=latin1";
         $db->query($sql2);
-        $sql3 = "insert into members (fname, lname, uname, pword, email, active) values ('Admin','Istrator','admin','0192023a7bbd73250516f069df18b500','admin@example.com','1')";
+        $sql3 =
+            "insert into members (fname, lname, uname, pword, email, active) values ('Admin','Istrator','admin','0192023a7bbd73250516f069df18b500','admin@example.com','1')";
         $db->query($sql3);
-        $sql4 = "create table if not exists `categories` (`id` int(11) not null auto_increment, `parent_id` int(11) not null, `name` varchar(64) not null, primary key (`id`))";
+        $sql4 =
+            "create table if not exists `categories` (`id` int(11) not null auto_increment, `parent_id` int(11) not null, `name` varchar(64) not null, primary key (`id`))";
         $db->query($sql4);
+        $sql6 = "insert into categories (parent_id, name) values (1, 'uncategorized')";
+        $db->query($sql6);
         $sql5 = "CREATE TABLE IF NOT EXISTS `bugs` (
 			`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
 			`author` varchar(250) DEFAULT NULL,
