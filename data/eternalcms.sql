fa52c492751d88a57515b1c173bc50b7a891df18
Jesse P Lesperance <jesse@jplesperance.me>
2012-04-29 21:19:30 -0700

    Moved the sql dump file to the data folder. added new Ant scripts

---
+++ data/eternalcms.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 3.1.3
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 06, 2009 at 11:44 PM
+-- Server version: 5.1.32
+-- PHP Version: 5.2.9-1
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `eternalcms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `members`
+--
+
+CREATE TABLE IF NOT EXISTS `members` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fname` varchar(25) NOT NULL,
+  `lname` varchar(50) NOT NULL,
+  `uname` varchar(25) NOT NULL,
+  `pword` varchar(255) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `active` set('0','1') NOT NULL DEFAULT '0',
+  `joined` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `news`
+--
+
+CREATE TABLE IF NOT EXISTS `news` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(64) NOT NULL,
+  `summary` varchar(255) NOT NULL,
+  `content` longtext NOT NULL,
+  `author` varchar(128) NOT NULL,
+  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `views` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `title` (`title`,`summary`,`author`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COMMENT='Holds News story meta data' AUTO_INCREMENT=24 ;
+
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_id` INT(11) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM DEFAULT CHARSET-latin1 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `bugs` (
+`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+`author` varchar(250) DEFAULT NULL,
+`email` varchar(250) DEFAULT NULL,
+`date` int(11) DEFAULT NULL,
+`url` varchar(250) DEFAULT NULL,
+`description` text,
+`priority` varchar(50) DEFAULT NULL,
+`status` varchar(50) DEFAULT NULL,
+PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET-latin1 AUTO_INCREMENT=1 ;
